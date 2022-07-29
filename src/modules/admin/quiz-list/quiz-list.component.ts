143e0e3c223fc65ec40bca3ef70f8c0e66ab3ffb
Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>
2022-07-29 18:37:05 +0430

    feat(front): set page s as rtl and improvement ui of grid

--- src/modules/admin/quiz-list/quiz-list.component.ts
+++ src/modules/admin/quiz-list/quiz-list.component.ts
@@ -5,15 +5,43 @@ import { AdminService } from 'src/services/admin/admin.service';
 @Component({
   selector: 'app-quiz-list',
   templateUrl: './quiz-list.component.html',
-  styleUrls: ['./quiz-list.component.scss']
+  styleUrls: ['./quiz-list.component.scss'],
 })
 export class QuizListComponent implements OnInit {
+  setCellStyles(value: string) {
+    if (value.endsWith('✅')) {
+      //mark police cells as red
+      return { color: 'white', backgroundColor: '#2aab2a' };
+    }
+    return null;
+  }
   columnDefs = [
-    { headerName: 'سوال', field: 'questionText' },
-    { headerName: 'گزینه اول', field: 'option1' },
-    { headerName: 'گزینه دوم', field: 'option2' },
-    { headerName: 'گزینه سوم', field: 'option3' },
-    { headerName: 'گزینه چهارم', field: 'option4' },
+    {
+      headerName: 'سوال',
+      field: 'questionText',
+      flex: 1,
+      cellStyle: { color: 'black', backgroundColor: '#b6b0b0' },
+    },
+    {
+      headerName: 'گزینه اول',
+      field: 'option1',
+      cellStyle: (params: any) => this.setCellStyles(params.value),
+    },
+    {
+      headerName: 'گزینه دوم',
+      field: 'option2',
+      cellStyle: (params: any) => this.setCellStyles(params.value),
+    },
+    {
+      headerName: 'گزینه سوم',
+      field: 'option3',
+      cellStyle: (params: any) => this.setCellStyles(params.value),
+    },
+    {
+      headerName: 'گزینه چهارم',
+      field: 'option4',
+      cellStyle: (params: any) => this.setCellStyles(params.value),
+    },
   ];
 
   rowData: any[] = [];
@@ -30,6 +58,9 @@ export class QuizListComponent implements OnInit {
         let index = 1;
         question.options.forEach((option: QuestionOption) => {
           row['option' + index] = option.optionText;
+          if (option.isAnswer) {
+            row['option' + index] += '✅';
+          }
           ++index;
         });
         this.rowData.push(row);
