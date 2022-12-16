e79419732fb418c817fff2e43c69c336481496f0
Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>
2022-12-16 19:17:24 +0330

    feat: add validate token

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from 'src/quards/auth.guard';
+import { ValidateTokenGuard } from 'src/quards/validateToken.guard';
 import { ContainerComponent } from './components/container/container.component';
 
 const routes: Routes = [
@@ -8,13 +9,13 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('../modules/main/main.module').then((m) => m.MainModule),
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard, ValidateTokenGuard],
   },
   {
     path: 'admin',
     loadChildren: () =>
       import('../modules/admin/admin.module').then((m) => m.AdminModule),
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard, ValidateTokenGuard],
   },
   { path: 'login', component: ContainerComponent },
 ];
