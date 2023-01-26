b5a5cfaa756e2ecd7fc7116093c814411c93649b
Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>
2023-01-26 08:22:50 +0330

    feat: add category module to nestjs
    Signed-off-by: Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>

--- quiz/src/category/category.controller.ts
+++ quiz/src/category/category.controller.ts
@@ -8,9 +8,42 @@ import {
   Delete,
 } from '@nestjs/common';
 import { catchError, map, Observable, of } from 'rxjs';
+import { CategoryRepository } from './category.repository';
 import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
-@Controller('auth')
+@Controller('category')
 export class CategoryController {
   constructor(private categoryService: CategoryService) {}
+
+  @Post()
+  async createCategory(
+    @Body() body: CreateCategoryDto,
+  ): Promise<{ updated?: boolean; created?: boolean; message: string }> {
+    return await this.categoryService.create(body.title);
+  }
+
+  @Put(':id')
+  async updateCategory(
+    @Param('id') id: string,
+    @Body() body: UpdateCategoryDto,
+  ) {
+    return this.categoryService.update(id, body.title);
+  }
+
+  @Get()
+  async getAllCategories(): Promise<CategoryRepository[]> {
+    return await this.categoryService.getAll();
+  }
+
+  @Get(':id')
+  async getCategoryById(@Param('id') id: string): Promise<CategoryRepository> {
+    return await this.categoryService.getById(id);
+  }
+
+  @Delete(':id')
+  async deleteCategoryById(@Param('id') id: string) {
+    return await this.categoryService.deleteById(id);
+  }
 }
--- quiz/src/category/category.module.ts
+++ quiz/src/category/category.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryController } from './category.controller';
+import { CategoryRepository } from './category.repository';
+import { CategoryService } from './category.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([CategoryRepository])],
+  controllers: [CategoryController],
+  providers: [CategoryService],
+})
 export class CategoryModule {}
